1cffc1af58db1be1e1697349f73e8ada5e444bbe
Artem Dubovov <artem.dubovov@bk.ru>
2022-07-25 13:44:13 +0300

    feat: finish task

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -177,8 +177,14 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  // if ((circle.center.x + circle.radius / 2 < point.x
+  // && circle.center.x - circle.radius / 2 < point.x)
+  // || (circle.center.y + circle.radius / 2 < point.y
+  // && circle.center.y - circle.radius / 2 < point.y)) {
+  //   return false;
+  // }
+  return (point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2 < circle.radius ** 2;
 }
 
 
@@ -421,6 +427,8 @@ function toNaryString(num, n) {
  */
 function getCommonDirectoryPath(/* pathes */) {
   throw new Error('Not implemented');
+  // const arr = pathes.map((el) => el.split('/'));
+  // return arr;
 }
 
 
